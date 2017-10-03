74a35e85363e3aa23af29266de6942ded98ec707
Amarii <amygeelhoed@gmail.com>
2017-10-03 15:01:13 +0200

    Added pump and button functions

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,12 +1,17 @@
 // Incluse I2C lib
 #include <Wire.h>
-
+ int buttonValue = 0;
 // Define the I2C to listen for the pi on
 #define SLAVE_ADDRESS 0x04
+int pompPin = A0;
+int buttonPin = 2;
+bool pompAllow;
 
 void setup(void) {
   Serial.begin(9600);
-
+  pinMode(A0, OUTPUT);
+  pinMode(buttonPin, INPUT);
+  
   // Start listening for I2C comm
   Wire.begin(SLAVE_ADDRESS);
 
@@ -14,10 +19,21 @@ void setup(void) {
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
 
-  Serial.println("Gestart");
+ // Serial.println("Gestart");
 }
 
 void loop(void) {
+  buttonValue = digitalRead(buttonPin);
+  
+if(buttonValue == 1 && pompAllow){
+
+  digitalWrite(pompPin, HIGH);
+ 
+  }
+else{
+  digitalWrite(pompPin, LOW);
+  
+  }
 
 }
 
@@ -25,12 +41,23 @@ void loop(void) {
 // We need to keep receiveing the data because the I2C connection will close otherwise
 void receiveData(int byteCount) {
   while (Wire.available()) {
-    Serial.println(Wire.read());
-    Serial.println("Gestart");
+   Serial.println(Wire.read());
+   int wireRead = Wire.read();
+  if(wireRead == 3){
+
+    pompAllow = true;
+    
+    }
+    else{
+      pompAllow = false;
+      }
+  
   }
+  
 }
 
 // When we're allowed to send data
 void sendData() {
-    Wire.write(2);
+  Serial.println(buttonValue);
+    Wire.write(buttonValue);
 }
